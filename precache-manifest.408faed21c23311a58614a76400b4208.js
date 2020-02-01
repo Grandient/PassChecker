0011787d90b752245bc37a6dc529d71b83b1ac11
Gavin <gavin.gosling@uoit.net>
2020-02-01 02:28:09 -0500

    Updates

---
+++ precache-manifest.408faed21c23311a58614a76400b4208.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3577e402f5793dece2dc8c084b8e224",
+    "url": "/Grandient.github.io-react/index.html"
+  },
+  {
+    "revision": "57e5eb6ba71eab271d72",
+    "url": "/Grandient.github.io-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/Grandient.github.io-react/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Grandient.github.io-react/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e5eb6ba71eab271d72",
+    "url": "/Grandient.github.io-react/static/js/main.179be9a1.chunk.js"
+  },
+  {
+    "revision": "5d4cac76d9004027295e",
+    "url": "/Grandient.github.io-react/static/js/runtime-main.4b372169.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Grandient.github.io-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
